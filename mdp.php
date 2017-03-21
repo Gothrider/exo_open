d09045c5e07d40c63968d94bc54b00b568c9f057
Gothrider <romain.olive.pro@gmail.com>
2017-03-22 00:05:30 +0100

    recherche de solution sur erreur de chargement de page

--- mdp.php
+++ mdp.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<title>Codes</title>
-	</head>
-	<body>
-	<p>
 <?php
 
-/*$speudo="Gothrider";
-$pass=3103;
-*/
-if ($_speudo=="Gothrider" && $_pass==3103)
+$_GET[speudo]=speudos;
+$_GET[pass]=mdp;
+
+if (speudos=="Gothrider"] && mdp==3103)
 {
 	echo "les codes sont 15A4D355";
 }
@@ -20,7 +12,5 @@ else
 	echo 'Votre login ou votre mot de passe est incorrect veuillez réessayer';
 	echo 'cliquez <a href="formulaire.php"> ICI</a>';
 }
+
 ?>
-</p>
-</body>
-</html>
