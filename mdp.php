6fe2e36899d1278afa52da8f84a518b2ecd9d11b
Gothrider <romain.olive.pro@gmail.com>
2017-03-21 23:45:38 +0100

    Création de la page code

---
+++ mdp.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Codes</title>
+	</head>
+	<body>
+	<p>
+<?php
+
+/*$speudo="Gothrider";
+$pass=3103;
+*/
+if ($_speudo=="Gothrider" && $_pass==3103)
+{
+	echo "les codes sont 15A4D355";
+}
+else
+{
+	echo 'Votre login ou votre mot de passe est incorrect veuillez réessayer';
+	echo 'cliquez <a href="formulaire.php"> ICI</a>';
+}
+?>
+</p>
+</body>
+</html>
